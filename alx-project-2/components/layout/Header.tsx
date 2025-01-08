58cb613463b4f76e19db8274bb79acb384bc6dbb
Dagi <dagmawimilkias@gmail.com>
2025-01-08 04:48:04 +0300

    Modified Header.tsx

--- alx-project-2/components/layout/Header.tsx
+++ alx-project-2/components/layout/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div>
       <ul>
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/home">Home</Link>
         </li>
         <li>
           <Link href="/about">About</Link>
